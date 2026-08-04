d4e2ffca44e61dba1337e4efd23606de6c0bbc48
John Beneke <johnbeneke@gmail.com>
2026-08-05 00:02:27 +0200

    Update

--- main.go
+++ main.go
@@ -7,11 +7,17 @@ import (
 )
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Hello, World!")
+	fmt.Fprintln(w, "Hello, World 1!")
+}
+
+func healthzHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintln(w, "ok")
 }
 
 func main() {
 	http.HandleFunc("/", helloHandler)
+	http.HandleFunc("/healthz", healthzHandler)
 
 	log.Println("Starting server on :8080")
 	if err := http.ListenAndServe(":8080", nil); err != nil {

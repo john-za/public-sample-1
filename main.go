5ddc01f05b52e7b6aa4eb2547c23667555989430
John Beneke <johnbeneke@gmail.com>
2026-08-08 16:30:26 +0200

    It works

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Hello, World 1!")
+	fmt.Fprintln(w, "Hello, World. It works!")
 }
 
 func healthzHandler(w http.ResponseWriter, r *http.Request) {
